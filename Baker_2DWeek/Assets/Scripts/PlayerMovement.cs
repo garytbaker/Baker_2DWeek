71ccb96d6c1b97fbad93e7002e7bbbd4b86eb809
garytabker <GeeBake@outlook.com>
2020-10-27 13:58:04 -0400

    working on horizontal movement

---
+++ Baker_2DWeek/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rB2D;
+
+    public float runSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontaInput = Input.GetAxis("Horizontal");
+        rB2D.velocity = 
+    }
+}
